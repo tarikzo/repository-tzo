6fc801f67016a3e195702a6b6a38a2e9bdb2e241
ZOUITNTA <tarik.zouitni@gmail.com>
2016-09-15 14:13:29 +0200

    Ajout de la classe1

---
+++ WpfApplication1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Class1
+    {
+
+        string chaine;
+
+        Class1()
+        {
+            chaine = "initialisation";
+            var titi = new StringBuilder();
+
+            titi.Append(chaine);
+
+            Console.WriteLine(titi.ToString());
+        }
+    }
+}
